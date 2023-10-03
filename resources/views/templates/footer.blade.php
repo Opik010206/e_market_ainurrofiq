085d034646ad88af45d0d3edf3594659fd555a4e
Ainur Rofiq <rofik010206@gmail.com>
2023-10-03 08:16:18 +0700

    ngubah footer dan layout

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -24,6 +24,7 @@
 <script src="{{ asset('bootsrapt5') }}/js/sweetalert.js"></script>
 <script src="{{ asset('adminlte3/plugins') }}/datatables/jquery.dataTables.min.js"></script>
 <script src="{{ asset('adminlte3/plugins') }}/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js" integrity="sha512-KBeR1NhClUySj9xBB0+KRqYLPkM6VvXiiWaSz/8LCQNdRpUm38SWUrj0ccNDNSkwCD9qPA4KobLliG26yPppJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 @stack('script')
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -13,6 +13,7 @@
   <link rel="stylesheet" href="{{ asset('adminlte3') }}/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/css/adminlte.min.css" integrity="sha512-IuO+tczf4J43RzbCMEFggCWW5JuX78IrCJRFFBoQEXNvGI6gkUw4OjuwMidiS4Lm9Q2lILzpJwZuMWuSEeT9UQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   @stack('style')
 </head>
 <body class="hold-transition sidebar-mini">
